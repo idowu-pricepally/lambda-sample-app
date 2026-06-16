f1a4074766f387de3a1fc9fe48246c33cd1ca4cb
idowu.pricepally <idowu.pricepally@gmail.com>
2026-06-16 18:38:27 +0100

    add role to existing provider

--- tofu/live/ci-cd-permissions/outputs.tf
+++ tofu/live/ci-cd-permissions/outputs.tf
@@ -2,11 +2,3 @@ output "arn" {
   description = "The Amazon Resource Name (ARN) specifying the OIDC provider"
   value       = module.github_oidc_iam_role.arn
 }
-
-################################################################################
-# OIDC Provider
-################################################################################
-output "url" {
-  description = "The URL of the identity provider. Corresponds to the iss claim"
-  value       = module.github_oidc_iam_provider.url
-}
